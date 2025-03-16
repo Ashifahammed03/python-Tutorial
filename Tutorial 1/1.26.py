953834b2d51b103305d33dc247f9e8787f7a5b22
Ashif Ahammed <ashifahammed842@gmail.com>
2025-03-16 22:40:27 +0530

    Update 1.26.py

--- Tutorial 1/1.26.py
+++ Tutorial 1/1.26.py
@@ -3,10 +3,9 @@ import math
 def distance(x1, y1, x2, y2):
     return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
-x1, y1 = map(float, input("Enter the coordinates of the first point (x1 y1): ").split())
-x2, y2 = map(float, input("Enter the coordinates of the second point (x2 y2): ").split())
+x1 = float(input("Enter x1: "))
+y1 = float(input("Enter y1: "))
+x2 = float(input("Enter x2: "))
+y2 = float(input("Enter y2: "))
 
-dist = distance(x1, y1, x2, y2)
-
-print(f"The distance between the points ({x1}, {y1}) and ({x2}, {y2}) is: {dist}")
-
+print("Distance:", distance(x1, y1, x2, y2))
