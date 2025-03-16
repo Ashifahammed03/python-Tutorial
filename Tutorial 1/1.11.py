8ae3a0484e1484d49e21bcd30bd8b88219b52af3
Ashif Ahammed <ashifahammed842@gmail.com>
2025-03-16 21:45:33 +0530

    Update 1.11.py

--- Tutorial 1/1.11.py
+++ Tutorial 1/1.11.py
@@ -2,7 +2,7 @@
 def sum_of_cubes_of_even_numbers(n):
     total = 0
     for i in range(2, n+1, 2): 
-        total += i**3  # Add the cube of the number
+        total += i**3 
     return total
 
 n = int(input("Enter a positive integer: "))
