03ee7d948c31997add6ac9e80d4035e123900cf2
Ashif Ahammed <ashifahammed842@gmail.com>
2025-03-16 20:47:40 +0530

    Update 1.8.py

--- Tutorial 1/1.8.py
+++ Tutorial 1/1.8.py
@@ -1,8 +1,8 @@
 def sum_of_digits(number):
     total = 0
     while number > 0:
-        total += number % 10  # Add the last digit
-        number //= 10  # Remove the last digit
+        total += number % 10  
+        number //= 10 
     return total
 
 number = int(input("Enter a number: "))
