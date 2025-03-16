2a40896c16c814d49588ae241215d7961aa65699
Ashif Ahammed <ashifahammed842@gmail.com>
2025-03-16 21:59:11 +0530

    Update 1.13.py

--- Tutorial 1/1.13.py
+++ Tutorial 1/1.13.py
@@ -2,9 +2,9 @@
 def reverse_number(number):
     reversed_num = 0
     while number > 0:
-        digit = number % 10  # Extract the last digit
+        digit = number % 10  
         reversed_num = reversed_num * 10 + digit 
-        number //= 10  # Remove the last digit
+        number //= 10  
     return reversed_num
 
 num = int(input("Enter a number: "))
